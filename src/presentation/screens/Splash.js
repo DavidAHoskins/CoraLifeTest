ba122f27cdd6f3e75b54303eb20c9999e35c8afa
David Hoskins <davidhoskins@imagineear.com>
2021-09-14 20:09:43 +0100

    Task 02 fix width and height to be the same as window width /2

--- src/presentation/screens/Splash.js
+++ src/presentation/screens/Splash.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import {StyleSheet, Image} from 'react-native';
+import {StyleSheet, Image,Dimensions} from 'react-native';
 import {useQuery} from '@apollo/client';
 import {useNavigation, CommonActions} from '@react-navigation/native';
 import {SafeAreaView} from 'react-native-safe-area-context';
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
 export const Splash = () => {
   const {data} = useQuery(GET_PARTICIPANTS);
   const navigation = useNavigation();
-
+  const windowWidth = Dimensions.get('window').width;
   useEffect(() => {
     if (data && data.characters) {
       setTimeout(() => {
@@ -32,7 +32,7 @@ export const Splash = () => {
   /*TODO TASK 08*/
   return (<SafeAreaView style={styles.container} edges={['right', 'left']} >
     <Image 
-      style={{flex:1, width:'90%', margin:'5%'}}
+      style={{flex:1, width:windowWidth/2, height:windowWidth/2, margin:windowWidth/4}}
       resizeMode="contain"
       resizeMethod="resize"
       source={require('../../../assets/images/splash.png')}
